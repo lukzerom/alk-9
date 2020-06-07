3e2f8177ce58e67be1c25609b50561b1ec7fb4b4
medynski <wojciech@medynski.io>
2020-06-07 13:29:47 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const httpServer = http.Server(app);
+
+app.get('/api', (req, res) => {
+  res.json({ status: 'Server works!' });
+});
+
+httpServer.listen(PORT, () => console.warn(`Listening on port: ${PORT}`));
