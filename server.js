613a1abf968cb5a5c62f629761d6c22a5ed05919
Lukasz <lukzerom@gmail.com>
2020-06-07 14:40:39 +0200

    Updated before build

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
-const http = require('http');
+const http = require('http');;
+const path = require('path')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const httpServer = http.Server(app);
+const httpServer = http.Server(app)
+
+app.use(cors())
+
 
 app.get('/api', (req, res) => {
-  res.json({ status: 'Server works!' });
+  res.json({
+    status: 'Server works!'
+  });
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json({
+    posts: 'Posts!'
+  });
+});
+
+// serve static react app
+app.use('/', express.static(path.resolve(path.dirname(''), './client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(path.dirname(''), './client/build/index.html'));
 });
 
 httpServer.listen(PORT, () => console.warn(`Listening on port: ${PORT}`));
